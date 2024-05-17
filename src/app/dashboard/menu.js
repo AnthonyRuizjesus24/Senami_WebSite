e347335fb3e329f47c013801c85eea39c7bd11bb
Max Ruiz <maxruizjesus40@gmail.com>
2024-05-17 11:35:07 -0500

    mejorarinterface

--- src/app/dashboard/menu.js
+++ src/app/dashboard/menu.js
@@ -1,7 +1,36 @@
-export default function Menu(){
-    return(
-        <nav className="menu">
-
-        </nav>
-    )
-}
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faDashboard, faBuilding, faWater, faCloud } from '@fortawesome/free-solid-svg-icons';
+export default function Menu() {
+    return (
+      <nav className="menu">
+        <ul>
+          <li>
+            <Link href="/">
+              Main <FontAwesomeIcon icon={faHome} className="menu-icon" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/dashboard">
+              Dashboard <FontAwesomeIcon icon={faDashboard} className="menu-icon" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/estaciones-automaticas">
+              Estaciones-Automaticas <FontAwesomeIcon icon={faBuilding} className="menu-icon" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/hidrologia">
+              Hidrologia <FontAwesomeIcon icon={faWater} className="menu-icon" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/meteorologia">
+              Meteorologia <FontAwesomeIcon icon={faCloud} className="menu-icon" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 import Layout from './layout';
+import Menu from './menu';
 export default function DashboardPage() {
-    return (
-        <Layout>
-        <div className="vertical-container">
-          <p>¡Hola desde la página dashboardPage!</p>
-          <button className="custom-button">
-            <Link href="/">
-              Ir al Main
-            </Link>
-          </button>
-        </div>
-      </Layout>
-    );
+  return (
+    <Layout>
+      <Menu />
+      <div className="vertical-container">
+        <p>¡Hola desde la página dashboardPage!</p>
+        <button className="custom-button">
+          <Link href="/">
+            Ir al Main
+          </Link>
+        </button>
+      </div>
+    </Layout>
+  );
 }
